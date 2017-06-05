ae38160c8ee42dc06d36c83169c9d0da2731da7a
Alaeddinalhamoud <eng.aladinhamod@gmail.com>
2017-06-05 16:03:12 +0100

    Actions
    Actions

--- sidebarNav/app/src/main/java/com/a_hamoud/sidebanav/MainActivity.java
+++ sidebarNav/app/src/main/java/com/a_hamoud/sidebanav/MainActivity.java
@@ -1,26 +1,49 @@
 package com.a_hamoud.sidebanav;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
-
+private NavigationView mNavView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mDrawerLayout=(DrawerLayout)findViewById(R.id.drawerlayout);
         mToggle=new ActionBarDrawerToggle(this,mDrawerLayout,R.string.open,R.string.close);
+        mNavView=(NavigationView)findViewById(R.id.NavView) ;
 
         mDrawerLayout.addDrawerListener(mToggle);
         mToggle.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mNavView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(final MenuItem item) {
+                if(item.getItemId()==R.id.nav_account){
+                    Toast.makeText(MainActivity.this, "Nav Account", Toast.LENGTH_SHORT).show();
+                    return  true;
+                } else if(item.getItemId()==R.id.nav_logout){
+                    Toast.makeText(MainActivity.this, "Nav Logout", Toast.LENGTH_SHORT).show();
+                    return  true;
+                }else{
+
+                    Toast.makeText(MainActivity.this, "Nav Setting", Toast.LENGTH_SHORT).show();
+                }
+
+                return false;
+            }
+        });
+
     }
 
     @Override
